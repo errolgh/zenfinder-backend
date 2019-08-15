7febc412392e8d736a0e53d5bb4318c9bc7cb075
errolgh <ewatsonrei@gmail.com>
2019-08-15 17:00:46 -0400

    fully serialized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    render json: User.find(params[:id]).to_json(default)
+  end
+
+  private
+
+  def default
+    {
+      :include => {
+        :reviews => {
+          :except => [:updated_at, :created_at]
+        },
+        :activities => {
+          :except => [:updated_at, :created_at]
+        }
+      }
+    }
+  end
 end
