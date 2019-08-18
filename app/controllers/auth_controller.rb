fdb260afcb47ad1ee831d00ac8dccebb54a6452e
errolgh <ewatsonrei@gmail.com>
2019-08-17 23:57:54 -0400

    working auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,21 @@
 class AuthController < ApplicationController
 
   def create
-
+    byebug
     @user = User.find_by(user_name: params[:user_name])
       if @user && @user.authenticate(params[:password])
-        render json: {user: @user}
+
+        payload = {"user_id": @user.id}
+        encode(payload)
+
+        token = encode(payload)
+        render json: {
+          user: @user,
+          token: token
+        }
       else
         render json: {message: "Incorrect username/password combination."}
       end
   end
 end
+#
